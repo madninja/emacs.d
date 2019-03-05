c124204548d0dd67e72efb349dd5db36e24489c3
Marc Nijdam <marc@helium.com>
2019-03-04 16:34:19 -0800

    Keychain, gpg setup

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,9 +3,6 @@
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
-(require-package 'nlinum)
-(add-hook 'prog-mode-hook 'nlinum-mode)
-
 (require-package 'protobuf-mode)
 (add-hook 'irony-mode-hook
           (lambda ()
@@ -36,5 +33,32 @@
               )
             )
   )
+
+
+;; From: https://emacs.stackexchange.com/a/32882
+;; Put this in your ~/.gnupg/gpg-agent.conf:
+;;
+;; allow-emacs-pinentry
+;; allow-loopback-pinentry
+;;
+;; Run:
+;;
+;; gpgconf --reload gpg-agent
+;;
+(maybe-require-package 'pinentry)
+(setq epa-pinentry-mode 'loopback)
+(pinentry-start)
+
+;; Use keychain to wrap ssh-agent. Put something like the following in
+;; your bashrc
+;;
+;; if [ -f /usr/bin/keychain ]; then
+;;     /usr/bin/keychain --gpg2 --quiet
+;;     source $HOME/.keychain/$HOSTNAME-sh
+;; fi
+;;
+(maybe-require-package 'keychain)
+(keychain-refresh-environment)
+
 
 (provide 'init-local)
