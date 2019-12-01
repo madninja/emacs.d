90ca44d57b533e858f677fb001b05ed9674f5ff1
Marc Nijdam <marc_nijdam@mac.com>
2019-12-01 05:46:09 -0700

    Add tide-mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -6,10 +6,38 @@
 (require-package 'protobuf-mode)
 (add-hook 'irony-mode-hook
           (lambda ()
-            (push 'protobuf-mode irony-supported-major-modes)
+            (cl-pushnew 'protobuf-mode irony-supported-major-modes)
             )
           )
-(maybe-require-package 'alchemist)
+
+(require-package 'tide)
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(require-package 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
